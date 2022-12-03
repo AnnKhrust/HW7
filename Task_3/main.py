b11729285305f5ef978bd8d533520c4da8a06c84
AnnKhrust <anna.khrust@gmail.com>
2022-12-03 10:42:35 +0300

    Домашнее задание Rython 7

---
+++ Task_3/main.py
@@ -0,0 +1,8 @@
+with open('file1.txt', 'r', encoding='utf-8') as f1:
+    file1 = f1.readlines()
+with open('file2.txt', 'r', encoding='utf-8') as f2:
+    file2 = f2.readlines()
+with open('file3.txt', 'r', encoding='utf-8') as f3:
+    file3 = f3.readlines()
+
+print(f2)
